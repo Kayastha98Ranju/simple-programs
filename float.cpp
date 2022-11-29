29a07c9bf566e0a47f28393b6549f5a042a07c6a
Ranju Kayastha <119415292+Kayastha98Ranju@users.noreply.github.com>
2022-11-29 17:20:31 +0545

    Add files via upload

---
+++ float.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+	float num=4;
+	printf ("%f", num);
+}
---
+++ swap.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main () 
+{
+  
+float a=4.8;
+float b=6.9;
+float c=5;
+c=a;
+a=b;
+b=c;
+printf ("%.2f\n", a);
+printf ("%.2f", b);
+
+} 
